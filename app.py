4b385de66daf51bbafed430a7b564baaef251aa1
“NaderASC20” <ndaruvala2346@yahoo.com>
2022-12-26 23:24:58 +0200

    Removed hello word resource

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-try:
-    from war_restful_api.src.resources.war import War
-    from war_restful_api.src.resources.user import User
-except:
-    pass
+from war_restful_api.src.resources.war import War
+from war_restful_api.src.resources.user import User
 from flask import Flask
 from flask_restful import Api
 from flask_pymongo import PyMongo, MongoClient
